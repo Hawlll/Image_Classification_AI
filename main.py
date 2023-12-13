821275e2635eb9a3921938d912e9ba7269950039
Franklin Collazo <franklinguitar2004@icloud.com>
2023-12-13 11:43:00 -0500

    Added .gitignore

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import numpy as np
 
 # Limit tensorflow's access to vram, preventing out of memory error
 gpus = tf.config.experimental.list_physical_devices("GPU")
+print(gpus)
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
